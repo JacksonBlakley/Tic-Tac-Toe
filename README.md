215bf330b0acdc6ea8bf1b51df52fecd528def1e
Jackson Blakley <jacksonblakley@gmail.com>
2020-05-09 12:09:29 -0500

    add final functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a tic tac toe machine that was created for an assignment in the odin project!
--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -7,20 +7,25 @@ const player = (name, symbol) => {
     return {name, symbol}
 }
 
-const player1 = player("jim", "O");
-const player2 = player("kyle", "X");
-
-
-
-console.log("gameboardArray = " + gameboard.array);
-console.log("player 1: " + Object.entries(player1));
-console.log("player 2: " + player2.name + player2.symbol);
+const player1 = player("player1", "O");
+const player2 = player("player2", "X");
 
 
 const displayController = (() => {
     const container = document.querySelector('#gameboard');
-    var currentSymbol = player1.symbol;
+    const winningText = document.querySelector("#winText");
+    var current = player1;
     var counter = 0;
+
+    function flipCurrent () {
+        if(current.symbol == player1.symbol) {
+        current = player2;
+        }
+        else {
+        current = player1;
+        }              
+
+    }
 
 
     for(var i = 0; i < 9; i++) {
@@ -37,20 +42,13 @@ const displayController = (() => {
           if(counter < 9 && div.innerHTML == "") {
               const textContent = document.createElement('div');
               textContent.classList = 'symbol';
-              textContent.textContent = currentSymbol;
+              textContent.textContent = current.symbol;
               div.appendChild(textContent);
-              gameboard.array[div.id] = currentSymbol;
+              gameboard.array[div.id] = current.symbol;
               counter++
-              if(currentSymbol == player1.symbol) {
-                currentSymbol = player2.symbol;
-              }
-              else {
-                currentSymbol = player1.symbol;
-              }
+              flipCurrent();
           } 
-          console.log(div.id);
-          console.log(gameboard.array);
-          console.log(counter)
+
            //check for victory in rows
             var r = 0;
             for(r = 0; r < 7; r = r+3) {
@@ -58,8 +56,9 @@ const displayController = (() => {
              var b = gameboard.array[r+1];
              var c = gameboard.array[r+2];
                 console.log("r = " + r);
-            if(a == b && b == c && a != " " && b != " " && c != " " ) {
-                console.log("row victory!");
+            if(a == b && b == c && a != " " && b != " " && c != " " && counter < 9) { 
+                flipCurrent();
+                winningText.innerHTML = `Row Victory by ${current.symbol}!`;
                 counter = 9;
                 }
             }
@@ -69,33 +68,37 @@ const displayController = (() => {
             var  d = gameboard.array[col];
              var e = gameboard.array[col+3];
              var f = gameboard.array[col+6];
-            console.log("c = " + col);
-            if(d == e && e == f && d != " " && e != " " && f != " ") {
-                console.log("column victory!");
+            if(d == e && e == f && d != " " && e != " " && f != " " && counter < 9) {
+                flipCurrent ();
+                winningText.innerHTML = `Column Victory by ${current.symbol}!`;
                 counter = 9;
                 }
             }
 
             //check for victory diagonally
-            if(gameboard.array[0] == gameboard.array[4] && gameboard.array[4] == gameboard.array[8] && gameboard.array[0] != " " && gameboard.array[4] != " " && gameboard.array[8] != " ") {
-                console.log("diagonal victory from top left!");
-                conter = 9;
+            if(gameboard.array[0] == gameboard.array[4] && gameboard.array[4] == gameboard.array[8] && gameboard.array[0] != " " && gameboard.array[4] != " " && gameboard.array[8] != " " && counter < 9) {
+               flipCurrent();
+                winningText.innerHTML = `Diagonal Victory From Top Left by ${current.symbol}!`;
+                counter = 9;
+
             }
             
-            else if (gameboard.array[2] == gameboard.array[4] && gameboard.array[4] == gameboard.array[6] && gameboard.array[2] != " " && gameboard.array[4] != " " && gameboard.array[6] != " ") {
+            else if (gameboard.array[2] == gameboard.array[4] && gameboard.array[4] == gameboard.array[6] && gameboard.array[2] != " " && gameboard.array[4] != " " && gameboard.array[6] != " " && counter < 9) {
+                flipCurrent();
+                winningText.innerHTML = `Diagonal Victory From Top Right by ${current.symbol}!`;
                 console.log("diagonal victory from top right!");
                 counter = 9;
             }
-
-
-
-
-
-
       })  
     })
-   
-
-
-
+    //event listener for clear button
+   const clearBtn = document.querySelector('#resetBtn');
+   clearBtn.addEventListener('click', () => {
+       allBox.forEach((div) => {
+           div.innerHTML = "";
+       })
+       winningText.innerHTML = "O goes first!";
+       counter = 0;
+       gameboard.array = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+   })
 })();
